2d7990e806d99c4a14b91c3d20dabe2943291b2e
Steve <asdjzh66@gmail.com>
2017-04-23 22:42:18 +1000

    Create 12-4-1.py

---
+++ 12-4-1.py
@@ -0,0 +1,37 @@
+def read_file_into_list():
+    fin = open('words.txt')
+    t = []
+    for line in fin:
+        word = line.strip()
+        t.append(word)
+    return t
+
+def map():
+    d = {}
+    for word in t:
+        l_word = list(word)
+        l_word.sort()
+        tuple_word = tuple(l_word)
+        if tuple_word in d:
+            l = d[tuple_word]
+            l.append(word)
+            d[tuple_word] = l
+        else:
+            l = []
+            l.append(word)
+            d[tuple_word] = l
+    return d
+
+def find_anag():
+    vals = d.values()
+    for value in vals:
+        if len(value) >= 2:
+            print(value)
+    
+    
+    
+if __name__ == "__main__":
+    t =  read_file_into_list()
+    d = map()
+    find_anag()
+    
